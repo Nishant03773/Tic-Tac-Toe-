87a2d9bd486c90ef3c098b7b3d591678d02c9d3a
Nishant03773 <nishantrishi039@gmail.com>
2026-03-19 09:30:25 +0530

    Implement new model
    Previously the model was Logistic Regression. For more accuracy, I am using Decison Tree

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -47,7 +48,8 @@ y = data['Class']
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-model = LogisticRegression()
+# model = LogisticRegression() ---> Accuracy of the model slighlty lower than the decision tree
+model = DecisionTreeClassifier()
 model.fit(x_train,y_train)
 
 accuracy = model.score(x_test, y_test)
